898f905f5ce2e7cc349ac62369d57c38bc52627e
caelansar <819711623@qq.com>
2024-04-30 20:28:35 +0800

    feat: convert csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use caecli::{process_csv, Opts, SubCommand};
+use clap::Parser;
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
+        }
+    }
+
+    Ok(())
 }
