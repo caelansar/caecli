d496f11ba8b5d386ce2692c3fc5d1eeb236dca0f
caelansar <819711623@qq.com>
2024-05-24 22:21:35 +0800

    feat: init tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use caecli::{CmdExector, Opts};
 use clap::Parser;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
     let opts = Opts::parse();
     opts.cmd.execute().await?;
 
